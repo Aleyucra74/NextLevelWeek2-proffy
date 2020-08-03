a0d9a5007c3b51eef13917b752965ad0b4624179
Alexandrey <alexandre.yucra@outlook.com>
2020-08-03 19:53:32 -0300

    item teacher style done

--- web/src/pages/TeacherList/index.tsx
+++ web/src/pages/TeacherList/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 import PageHeader from '../../components/PageHeader';
 
+import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
 import './styles.css';
 
 function TeacherList() {
@@ -26,6 +27,35 @@ function TeacherList() {
 
                 </form>
             </PageHeader>
+
+            <main>
+                <article className="teacher-item">
+                    <header>
+                        <img src="https://avatars0.githubusercontent.com/u/52496574?s=460&u=86fc6ef72a87c961c622eed233423e7cae217d52&v=4" alt="Alexandre yucra"/>
+                        <div>
+                            <strong>Alexandre yucra</strong>
+                            <span>Math</span>
+                        </div>
+                    </header>
+
+                    <p>
+                        Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
+                            <br/><br/>
+                        Non incidunt libero exercitationem saepe nulla repellendus omnis
+                        repudiandae consequatur ex adipisci praesentium, inventore sunt, eaque beatae! Magni porro eveniet sed sequi?    
+                    </p>                   
+                    <footer>
+                        <p>
+                            Preço Hora
+                            <strong> R$ 100,00</strong>
+                        </p>
+                        <button type="button">
+                            <img src={whatsappIcon} alt="whatsapp"/>
+                            Entrar em contato
+                        </button>
+                    </footer>
+                </article>
+            </main>
         </div>
     )
 }
