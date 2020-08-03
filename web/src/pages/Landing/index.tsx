8b031f3923eec8fee8a50c942fa8d622f4d810e3
Alexandrey <alexandre.yucra@outlook.com>
2020-08-03 13:37:29 -0300

    desktop layout done

--- web/src/pages/Landing/index.tsx
+++ web/src/pages/Landing/index.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import './styles.css';
 
 import logoImg from '../../assets/images/logo.svg';
 import landingImg from '../../assets/images/landing.svg';
@@ -7,6 +6,8 @@ import landingImg from '../../assets/images/landing.svg';
 import studyIcon from '../../assets/images/icons/study.svg';
 import giveClassesIcon from '../../assets/images/icons/give-classes.svg';
 import purpleHeartIcon from '../../assets/images/icons/purple-heart.svg';
+
+import './styles.css';
 
 function Landing() {
     return (
@@ -26,8 +27,8 @@ function Landing() {
                     </a>
 
                     <a href="" className="give-classes">
-                        <img src={giveClassesIcon} alt="Estudar"/>
-                        Estudar
+                        <img src={giveClassesIcon} alt="Dar aulas"/>
+                        Dar aulas
                     </a>
                 </div>
 
