1852cb34800e98584500b4f1981fd4f306796d79
Alexandrey <alexandre.yucra@outlook.com>
2020-08-05 13:37:25 -0300

    form with components done

--- web/src/pages/TeacherForm/index.tsx
+++ web/src/pages/TeacherForm/index.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
 
 import PageHeader from '../../components/PageHeader';
+import Input from '../../components/Input';
+import warningIcon from '../../assets/images/icons/warning.svg';
+import Textarea from '../../components/Textarea';
+import Select from '../../components/Select';
 
 import './styles.css';
 
@@ -12,24 +16,67 @@ function TeacherForm() {
             <main>
                 <fieldset>
                     <legend>Seus dados</legend>
+                    <Input name="name" label="Nome completo"/>
+                    <Input name="avatar" label="Avatar"/>
+                    <Input name="whatsapp" label="Whatsapp"/>
+                    <Textarea name="bio" label="Biografia"/>
+                </fieldset>
 
-                    <div className="input-block">
-                        <label htmlFor="name">Nome completo</label>
-                        <input type="text" id="name"/>
+                <fieldset>
+                    <legend>Sobre a aula</legend>
+
+                    <Select 
+                        name="subject" 
+                        label="Matéria"
+                        options={[
+                            { value: 'Artes', label: 'Artes'},
+                            { value: 'math', label: 'math'},
+                            { value: 'biologia', label: 'biologia'},
+                            { value: 'port', label: 'port'},
+                            { value: 'eduf', label: 'eduf'},
+                        ]}
+                    />
+                    <Input name="cost" label="Custo da sua hora por aula"/>
+                </fieldset>
+                
+                <fieldset>
+                    <legend>
+                        Horarios disponiveis
+                        <button type="button">
+                            + Novo Horario
+                        </button>
+                    </legend>
+
+                    <div className="schedule-item">
+                        <Select 
+                            name="week_day" 
+                            label="Dia da semana"
+                            options={[
+                                { value: '0', label: 'Domingo'},
+                                { value: '1', label: 'Segunda'},
+                                { value: '2', label: 'Terça'},
+                                { value: '3', label: 'Quarta'},
+                                { value: '4', label: 'Quinta'},
+                                { value: '5', label: 'Sexta'},
+                                { value: '6', label: 'Sabádo'},
+                            ]}
+                        /> 
+                        <Input name="from" label="Das" type="time"/>
+                        <Input name="to" label="Até" type="time"/>
                     </div>
-
-                    <div className="input-block">
-                        <label htmlFor="avatar">Avatar</label>
-                        <input type="text" id="avatar"/>
-                    </div>
-
-                    <div className="input-block">
-                        <label htmlFor="whatsapp">Whatsapp</label>
-                        <input type="text" id="whatsapp"/>
-                    </div>
-
 
                 </fieldset>
+
+                <footer>
+                    <p>
+                        <img src={warningIcon} alt="Aviso importante"/>
+                        Importante! <br/>
+                        Preencha todos os dados
+                    </p>
+                    <button type="button">
+                        Salvar cadastro
+                    </button>
+                </footer>
             </main>
                       
         </div>
--- web/src/pages/TeacherList/index.tsx
+++ web/src/pages/TeacherList/index.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 
 import PageHeader from '../../components/PageHeader';
-
-import './styles.css';
 import TeacherItem from '../../components/TeacherItem';
 import Input from '../../components/Input';
+import Select from '../../components/Select';
+
+import './styles.css';
 
 
 function TeacherList() {
@@ -12,8 +13,30 @@ function TeacherList() {
         <div id="page-teacher-list" className="container">
             <PageHeader title="Estes são os proffys disponiveis.">
                 <form id="search-teachers">
-                    <Input name="subject" label="Matéria"/>
-                    <Input name="week_day" label="Dia da semana"/>
+                    <Select 
+                        name="subject" 
+                        label="Matéria"
+                        options={[
+                            { value: 'Artes', label: 'Artes'},
+                            { value: 'math', label: 'math'},
+                            { value: 'biologia', label: 'biologia'},
+                            { value: 'port', label: 'port'},
+                            { value: 'eduf', label: 'eduf'},
+                        ]}
+                    /> 
+                    <Select 
+                        name="week_day" 
+                        label="Dia da semana"
+                        options={[
+                            { value: '0', label: 'Domingo'},
+                            { value: '1', label: 'Segunda'},
+                            { value: '2', label: 'Terça'},
+                            { value: '3', label: 'Quarta'},
+                            { value: '4', label: 'Quinta'},
+                            { value: '5', label: 'Sexta'},
+                            { value: '6', label: 'Sabádo'},
+                        ]}
+                    />
                     <Input type="time" name="time" label="Hora"/>
                 </form>
             </PageHeader>
