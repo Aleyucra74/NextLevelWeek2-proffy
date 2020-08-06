3129328745a62931392408f0d0e4dbce81ec7371
Alexandrey <alexandre.yucra@outlook.com>
2020-08-06 16:42:23 -0300

    navigation back

--- mobile/src/pages/GiveClasses/index.tsx
+++ mobile/src/pages/GiveClasses/index.tsx
@@ -1,8 +1,36 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, ImageBackground, Text } from 'react-native';
+
+import giveClassesBgImage from '../../assets/images/give-classes-background.png';
+import { RectButton } from 'react-native-gesture-handler';
+import { useNavigation } from '@react-navigation/native';
+
+import styles from './styles';
 
 function GiveClasses() {
-    return <View />
+    const { goBack } = useNavigation();
+
+    function handleNavigationBack() {
+        goBack();
+    }
+
+    return (
+        <View style={styles.container}>
+            <ImageBackground resizeMode="contain" source={giveClassesBgImage} style={styles.content}>
+
+                <Text style={styles.title}>Quer ser um proofy?</Text>
+                <Text style={styles.description}>
+                    Para começar, voce precisa se cadastrar como professor na nossa plataforma web
+                </Text>
+            </ImageBackground>
+
+            <RectButton onPress={handleNavigationBack} style={styles.okButton}>
+                <Text style={styles.okButtonText}>
+                    Tudo bem
+                </Text>
+            </RectButton>
+        </View>
+    );
 }
 
 export default GiveClasses;
